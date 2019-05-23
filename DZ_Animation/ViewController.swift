f97aa227e74262c8e8f644cfeeff0578194748e5
Sergey Koshlakov <appmaster@me.com>
2019-05-24 00:54:36 +0300

    added random animations

--- DZ_Animation/ViewController.swift
+++ DZ_Animation/ViewController.swift
@@ -10,14 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var superTestView: UIView!
+    
+    weak var testView: UIView!
+    
+    var testViewAnimator: UIViewPropertyAnimator?
+    
     var playButtonPressed = true
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let frame = CGRect(x: 0, y: 0, width: 50, height: 50)
+        let view = UIView(frame: frame)
+        view.backgroundColor = .red
+        superTestView.addSubview(view)
+        testView = view
+        
     }
     
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
+    func randomColor() -> UIColor {
+        let red = CGFloat(arc4random()) / CGFloat(UInt32.max)
+        let green = CGFloat(arc4random()) / CGFloat(UInt32.max)
+        let blue = CGFloat(arc4random()) / CGFloat(UInt32.max)
+        
+        return UIColor(red: red, green: green, blue: blue, alpha: 1)
     }
     
     func animationButtonPressed(button: UIButton, completion:@escaping () -> ()) -> UIViewPropertyAnimator {
@@ -36,7 +53,40 @@ class ViewController: UIViewController {
         return animator
     }
     
+    func moveView(_ view: UIView) {
+        
+        let rect = superTestView.bounds.insetBy(dx: testView.bounds.width, dy: testView.bounds.height)
+        
+        let randomX = CGFloat(arc4random_uniform(UInt32(rect.width))) + rect.minX
+        let randomY = CGFloat(arc4random_uniform(UInt32(rect.height))) + rect.minY
+        
+        let randomScaleX = CGFloat(arc4random_uniform(UInt32(1.5 * 100)) / 100) + CGFloat(0.5)
+        let randomScaleY = CGFloat(arc4random_uniform(UInt32(1.5 * 100)) / 100) + CGFloat(0.5)
+        
+        let parameter = UICubicTimingParameters(animationCurve: .linear)
+        
+        let animator = UIViewPropertyAnimator(duration: 3, timingParameters: parameter)
+        
+        animator.addAnimations {
+            self.testView.center = CGPoint(x: randomX, y: randomY)
+            self.testView.transform = CGAffineTransform(scaleX: randomScaleX, y: randomScaleY)
+            self.testView.backgroundColor = self.randomColor()
+        }
+        
+        animator.addCompletion { _ in
+            self.moveView(view)
+        }
+        
+        testViewAnimator = animator
+        
+        testViewAnimator!.startAnimation()
+        
+    }
+    
     @IBAction func actionBackwardButton(_ sender: UIButton) {
+        
+        testViewAnimator?.fractionComplete -= 0.2
+            
         let animator = animationButtonPressed(button: sender) {
             sender.transform = CGAffineTransform.identity
             sender.superview?.backgroundColor = .clear
@@ -45,6 +95,9 @@ class ViewController: UIViewController {
     }
 
     @IBAction func actionForwardButton(_ sender: UIButton) {
+        
+        testViewAnimator?.fractionComplete += 0.2
+            
         let animator = animationButtonPressed(button: sender) {
             sender.transform = CGAffineTransform.identity
             sender.superview?.backgroundColor = .clear
@@ -53,6 +106,16 @@ class ViewController: UIViewController {
     }
     
     @IBAction func actionPlayButton(_ sender: UIButton) {
+        
+        if testViewAnimator == nil {
+            moveView(testView)
+        } else if !self.playButtonPressed {
+            self.testViewAnimator?.pauseAnimation()
+        } else {
+            let newParameters = UICubicTimingParameters(animationCurve: .linear)
+            self.testViewAnimator?.continueAnimation(withTimingParameters: newParameters, durationFactor: 1)
+        }
+        
         let animator = animationButtonPressed(button: sender) {
             if self.playButtonPressed {
                sender.setImage(UIImage(named: "pause"), for: [])
@@ -64,6 +127,7 @@ class ViewController: UIViewController {
             
             self.playButtonPressed.toggle()
         }
+        
         animator.startAnimation()
     }
     
